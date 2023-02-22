37bc4ddd3e64e9789c33e683bcd77fd048612487
Nachtschrecken <ferris@uldagon.com>
2023-02-22 20:31:39 +0100

    added csv

--- app.R
+++ app.R
@@ -1,18 +1,25 @@
-# Load necessary libraries
 library(stringr)
 
-# Set the path to the folder containing the text files
+# Structure to load all files
 folder_path <- "dataset"
-
-# Get a list of all text files in the folder
 file_list <- list.files(folder_path)
 text_id <- 0
 
+# basic structure for data frames
+df <- data.frame(
+          doc_id=character(), 
+          text_id=character(),
+          title=character(), 
+          date=character(), 
+          text=character())
+
+# For loop over all text files
 for (file in file_list) {
   
   # Set the file path to the input text file
   input_file <- paste0("dataset/",file)
   
+  # text_id column
   text_id <- text_id + 1
   
   # Read in the text file as a single character string
@@ -21,15 +28,6 @@ for (file in file_list) {
   # Split the text into paragraphs using regex
   paragraphs <- str_split(text, "\\n\\n")
   
-  if (text_id == 1) {
-    df <- data.frame(
-              doc_id = 1:(length(unlist(paragraphs))-2), 
-              text_id = text_id,
-              title = str_split_fixed(text, "\\n\\n", 3)[,1], 
-              date = str_split_fixed(text, "\\n\\n", 3)[,2], 
-              text = unlist(paragraphs)[-c(1:2)])
-  }
-  
   # Create a data frame with the paragraph numbers and text
   df <- rbind(df, data.frame(
             doc_id = 1:(length(unlist(paragraphs))-2), 
@@ -37,12 +35,7 @@ for (file in file_list) {
             title = str_split_fixed(text, "\\n\\n", 3)[,1], 
             date = str_split_fixed(text, "\\n\\n", 3)[,2], 
             text = unlist(paragraphs)[-c(1:2)]))
-  
-  # Write the data frame to a CSV file
-  write.table(df, "paragraphs.csv", row.names = FALSE, append = TRUE)
 }
 
-
-
-
-
+# Write the data frame to a CSV file
+write.csv(df, "paragraphs.csv", row.names = FALSE)
